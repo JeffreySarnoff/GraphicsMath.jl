306e7306c922e35cce8d3124d7ecd3c27952de3e
Documenter.jl <documenter@juliadocs.github.io>
2022-07-24 12:13:33 +0000

    build based on f205ff2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GraphicsMath","category":"page"},{"location":"#GraphicsMath","page":"Home","title":"GraphicsMath","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GraphicsMath.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GraphicsMath]","category":"page"}]
+}
